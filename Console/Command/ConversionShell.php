4af50d7f1ca94397b61c6c0f3233c7117ad5d52f
Jorge B <jorge@artvisual.net>
2013-02-11 11:03:41 +0100

    error tamaños 3gp

--- Console/Command/ConversionShell.php
+++ Console/Command/ConversionShell.php
@@ -5,7 +5,7 @@ class ConversionShell extends AppShell {
 		'mp4' => array('folder' => 'mp4', 'sizes' => array('m', 's')),
 		'flv' => array('folder' => 'flv', 'sizes' => array('l', 'm', 's')), 
 		'wmv' => array('folder' => 'wmv', 'sizes' => array('l', 'm', 's')),
-		'v3gp' => array('folder' => '3gp', array('s'))
+		'v3gp' => array('folder' => '3gp', 'sizes' => array('s'))
 	);
 	
 	public function convert_all() {
